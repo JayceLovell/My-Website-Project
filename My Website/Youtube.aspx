282cc163959b7a2a0d7cb8f2fe8c215ac9aeed2f
Waynell Lovell <waynell_cookie@hotmail.com>
2018-02-05 22:19:58 -0500

    Started #2

--- My Website/Youtube.aspx
+++ My Website/Youtube.aspx
@@ -1,3 +1,13 @@
 ﻿<%@ Page Title="Youtube" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Youtube.aspx.cs" Inherits="My_Website.Youtube" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="contain">
+        <div class="row">
+            <div class="col-mid-offset-4 col-md-12">
+                <h1 class="center">Youtube</h1>
+                <div class="embed-responsive embed-responsive-16by9">
+                     <iframe id="YoutubeVideos" runat="server" title="Waynell_17 Channel Video" class="embed-responsive-item" src=""></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- My Website/Youtube.aspx.cs
+++ My Website/Youtube.aspx.cs
@@ -11,7 +11,52 @@ namespace My_Website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Random random = new Random();
+            switch (random.Next(0,15))
+            {
+                case 0:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/M6kBB50BvQY?autoplay=1&rel=0&showinfo=0";
+                break;
+                case 1:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/36mI3xVJ-Pw?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 2:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/Sxa-iv_iFWc?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 3:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/sxEn-Cztejk?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 4:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/wzG8gjUdALs?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 5:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/ZpY0KfISJ4k?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 6:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/SAvXqQRb06o?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 7:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/kK8MvTxClHc?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 8:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/2BHYMwwULmk?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 9:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/mNQYCWylcwM?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 10:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/ZH_bDItP_zg?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 11:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/GuPhHyUTpX8?autoplay=1&rel=0&showinfo=0";
+                    break;
+                case 12:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/fjA92IW5GUM?autoplay=1&rel=0&showinfo=0";
+                    break;
+                default:
+                    YoutubeVideos.Src = "https://www.youtube.com/embed/3bbq5E2LY0U?autoplay=1&rel=0&showinfo=0";
+                break;
+            }
         }
     }
 }
